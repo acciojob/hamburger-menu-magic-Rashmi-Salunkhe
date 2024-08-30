417f52f3fec789584c1d6c825c85f4bfabcb0190
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-30 12:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
